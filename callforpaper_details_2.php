688b03236457456b248ac73bfbd8b5a706dfc33f
mes_murugan <mes.murugan@gmail.com>
2021-10-01 22:20:24 +0530

    Easy Chair Updation

--- callforpaper_details_2.php
+++ callforpaper_details_2.php
@@ -3,31 +3,137 @@
 				<!-- Title -->
 				<div class="block text-justify">
 					<p style="color:#222222">
-						The Multidisciplinary International Conference on Humans and Technology: 
-						A Holistic and Symbiotic Approach to Sustainable Development, ICHT 2022, 
-						aims to bring together researchers, engineers, developers, and practitioners 
-						from academia and industry working in various areas of Computer Science, 
-						Electronics, Life Sciences, Physical and Mathematical Sciences, Humanities, 
-						Social Sciences, and Physical Education to share their experience, exchange 
-						and cross-fertilize their ideas.
+						The Multidisciplinary <b>International Conference on Humans and Technology: A Holistic 
+						and Symbiotic Approach to Sustainable Development, ICHT 2022</b>, aims to bring together 
+						researchers, engineers, developers, and practitioners from academia and industry 
+						working in various areas of Computer Science, Electronics and Instrumentation, Life 
+						Sciences, Physical and Mathematical Sciences, Humanities, Social Sciences, and Physical 
+						Education to share their experience, exchange and cross-fertilize their ideas.
 					</p>
-					<p style="color:#222222">
+					<p style="color:#222222; margin: 15px 0px 0px 0px;">
 						Authors are invited to submit their original and unpublished work on different 
-						tracks. The accepted and registered articles fulfilling publisher requirements 
-						on quality will be published in a UGC-approved journal (Approval pending). 
-						But, all the accepted and presented articles will be published in Fortis, 
-						the multi-disciplinary, peer-reviewed print research journal of MES College 
-						Marampally with International Standard Serial Number (ISSN) 2320-6989.  
-						At least one author must register for every paper that is to be included 
-						in the conference proceedings. Book of Abstracts (pdf) will be delivered to 
-						participants during the conference.
+						tracks. Selected articles <b>fulfilling publisher requirements on quality will be 
+						published</b> in a UGC-approved journal (Approval pending). But, all the accepted and 
+						presented articles will be published in Fortis, the multi-disciplinary, 
+						peer-reviewed print research journal of MES College Marampally with International 
+						Standard Serial Number (ISSN) 2320-6989. At least one author must register for 
+						every paper that is to be included in the conference proceedings. Book of 
+						Abstracts (pdf) will be delivered to participants during the conference.
 					</p>
-					<p style="color:#222222">
-						<b>The links for paper submission will be provided sooon.</b>
+					<p style="color:#222222; margin: 15px 0px 0px 0px;">
+						All submissions must be written in <b>English</b> and are limited to a maximum paper 
+						length of <b>eight (8) printed pages</b> (10-point font) including figures and references. 
+						The <b>Best Paper Award</b> will be conferred to the author of a paper to recognize and 
+						promote quality contributions to academic research and will be selected by 
+						Programme Committee. The decision criterion will consider both the paper quality 
+						and the oral presentation quality. The “Best Paper Award” will be given for each 
+						track.
 					</p>
-				</div>
-				<div class="sponsor-btn text-center">
-					<a href="#" class="btn btn-main-md">Registration</a>
 				</div>
 			</div>
 		</div>
+
+
+<!--=================================
+=            Testimonial            =
+==================================-->
+
+<section class="se ction testimonial">
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<div class="section-title">
+					<h3>ICHT 2022 <span class="alternate">Registration</span></h3>
+				</div>
+			</div>
+		</div>
+		<div class="row mt-20">
+			<div class="col-lg-4 col-md-6 mb-20">
+				<!-- Testimonial -->
+				<div class="testimonial-item">
+					<!-- Given Comment -->
+					<div class="comment">
+						<div class="widget category">
+							<!-- Widget Header -->
+							<h5 class="widget-header">Registration</h5>
+							<ul class="category-list m-0 p-0">
+								<li><a href="https://easychair.org/conferences/?conf=icht2022" target="_blank">Click here to Register<span class="float-right"></span></a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="person">
+						<div class="media">
+							<!-- Person Image -->
+							<img src="images/speakers/ec.jpg" alt="person-image">
+							<div class="media-body">
+								<!-- Person Name -->
+								<div class="name"><p>Easy Chair</p></div>
+								<!-- Profession -->
+								<div class="profession"><p></p></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-6">
+				<!-- Testimonial -->
+				<div class="testimonial-item">
+					<!-- Given Comment -->
+					<div class="comment">
+						<div class="widget category">
+							<!-- Widget Header -->
+							<h5 class="widget-header">Indian Delegates</h5>
+							<ul class="category-list m-0 p-0">
+								<li>Industry Experts<span class="float-right">4500(INR)</span></li>
+								<li>Academicians<span class="float-right">3500(INR)</span></li>
+								<li>Students/Research Scholars<span class="float-right">2500(INR)</span></li>
+								<li>Listeners<span class="float-right">1500(INR)</span></li>
+							</ul>
+						</div>
+					</div>
+					<div class="person">
+						<div class="media">
+							<!-- Person Image -->
+							<img src="images/speakers/logo_small.jpg" alt="person-image">
+							<div class="media-body">
+								<!-- Person Name -->
+								<div class="name"><p>Registration Fee</p></div>
+								<!-- Profession -->
+								<div class="profession"><p>(Publication Charges Extra)</p></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-6">
+				<!-- Testimonial -->
+				<div class="testimonial-item">
+					<!-- Given Comment -->
+					<div class="comment">
+						<div class="widget category">
+							<!-- Widget Header -->
+							<h5 class="widget-header">Foreign Delegates</h5>
+							<ul class="category-list m-0 p-0">
+								<li>International Delegates<span class="float-right">150 US$</span></li>
+							</ul>
+						</div>
+					</div>
+					<div class="person">
+						<div class="media">
+							<!-- Person Image -->
+							<img src="images/speakers/logo_small.jpg" alt="person-image">
+							<div class="media-body">
+								<!-- Person Name -->
+								<div class="name"><p>Registration Fee</p></div>
+								<!-- Profession -->
+								<div class="profession"><p>(Publication Charges Extra)</p></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<!--====  End of Testimonial  ====-->
