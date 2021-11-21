7d71e16f4b86325a6e100015b5adbf6452189dda
mes_murugan <mes.murugan@gmail.com>
2021-11-21 10:53:31 +0530

    Speaker Updation

---
+++ t6sp1.php
@@ -0,0 +1,52 @@
+<?php 
+$speakers = 'active';
+include 'header.php';?>
+<section class="section single-speaker">
+	<div class="container">
+		<div class="block">
+			<div class="row">
+				<div class="col-lg-5 col-md-6 align-self-md-center">
+					<div class="image-block">
+						<img src="images/speakers/s_t6sp1.jpg" class="img-fluid" alt="speaker">
+					</div>
+				</div>
+				<div class="col-lg-7 col-md-6 align-self-center">
+					<div class="content-block">
+						<div class="name">
+							<h5>Dr. TOOLIKA GUPTA</h5>
+						</div>
+						<div class="profession">
+							<p>Director<br>
+                            Indian Institute of Crafts and Design (IICD), Jaipur, Rajasthan, India<br>&<br>
+                            Secretary and Founder Member<br>
+                            Textiles and Clothing Research Centre (TCRC), India.
+                            </p>
+						</div>
+						<div class="details">
+							<p >Title of talk: 
+							<br>
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="block-2">
+			<div class="row">
+				<div class="col-md-6">
+					<div class="personal-info">
+						<h5>Personal Information</h5>
+                        <p>
+							Dr. Toolika Gupta has received PhD in History of Arts in Dress and Textiles from the University of Glasgow, UK, in 2016 and Master’s Degree in Textiles and Clothing from Lady Irwin College, Delhi University. She has amassed experience as a designer, consultant and academician in India and abroad. She also served as one of the ten ambassadors of the Costume Society of UK for 2016, Dean of Product Design and Crafts Design at the Rajasthan ILD Skills University since 2018. She is a member of CII Design Committee of 2019, 2020, and 2021, Member of FICCI Entrepreneurship Committee and Part of the Board of Studies of many design institutes. Areas of research includes Indian dress and textile history.
+							<br>
+						</p>
+						<p>
+							<br> 
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<?php include 'footer.php';?>
