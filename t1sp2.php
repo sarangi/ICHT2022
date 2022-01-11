ec7b166ba7082131dccaa7b21685e19d35b76ad6
mes_murugan <mes.murugan@gmail.com>
2022-01-11 14:20:19 +0530

    Updation on 11012022 at 14.20

--- t1sp2.php
+++ t1sp2.php
@@ -13,7 +13,7 @@ include 'header.php';?>
 				<div class="col-lg-7 col-md-6 align-self-center">
 					<div class="content-block">
 						<div class="name">
-							<h5>Dr. CELI1 SHEHNAZ</h5>
+							<h5>Dr. CELIA SHEHNAZ</h5>
 						</div>
 						<div class="profession">
 							<p>
@@ -37,10 +37,11 @@ include 'header.php';?>
 					<div class="personal-info">
 						<h5>Personal Information</h5>
                         <p>  
-							
+							Celia Shahnaz, SMIEEE, Fellow IEB, received Ph.D. degree from Concordia University, Canada, and is currently a Professor at, Department of EEE, BUET, Bangladesh, since 2015. She has published more than 150 international journal/conference papers. She is a recipient of the Canadian Commonwealth Scholarship/Fellowship and Bangladesh Academy of Science Gold Medal for her contribution to Science and Technology. Her research interests include the areas of signal processing for speech analysis and speech enhancement, audio-visual recognition for biometric security, control system, robotics, pattern recognition,  machine learning and deep learning for audio, video, biomedical, power signals, multimodal emotion recognition, and humanitarian technology.<br><br>Recently, her papers have received best paper awards in IEEE BECITHCON 2019,  biomedical Engineering tracks at TENCON 2017 and at IEEE WIECON-ECE 2016, in Humanitarian Challenge track at R10 HTC 2017, and the best interactive poster award at ICIVPR 2017. Her paper has been selected for the top ten best paper awards in the Student Paper Contest of the 2018 and 2014 IEEE International Midwest Symposium on Circuits and Systems (MWSCAS), College Station, Texas, USA and Knoxville, TN, USA, respectively. She was the winner, the Best Student Paper Award, 2008 IEEE International Conference on Neural Networks and Signal Processing (ICNNSP), Zhenjiang, China. She was selected as one of the finalists of the Student Research Presentation Competition in the 2009 SYTACOM Workshop, Montreal, QC, Canada.
 							<br>
 						</p>
 						<p>
+							She was the mentor of 1st/2nd prize winning projects in IEEE IAS CMD Contests (Robotics, Humanitarian) in 2017-2019, USA. She was the supervisor of 4th/5th rank winning teams in SPCUP competitions, ICASSP 2020 (Spain)/2015 (Australia) and 5th  rank winning team in VIP CUP competition, ICIP 2020 (United Arab Emirates). She has been elected as 2022 IEEE WIE Committee Chair-Elect and she will serve as the 2023-2024 IEEE WIE Committee Chair. She is the founder & Chair, IEEE SPS, IAS Bangladesh Chapters, Co-founder & Vice-Chair, IEEE RAS, SSIT Bangladesh Chapters, Founder and Advisor, WIE AG, IEEE Bangladesh Section. She has served as  the General Chair, IEEE TENSYMP 2020, IEEE BECITHCON 2021, IEEE SPICSCON 2019, IEEE PEEIACON 2021 & 2019, IEEE RAAICON 2021 General Chair: IEEE WIECON-ECE 2021, 2019, 2018, 2016, General Co- Chair, IEEE Region 10 HTC 2017 and IEEE WIECON-ECE 2017, Founder and  TPC Chair: IEEE SPICSCON 2021, IEEE WIECON-ECE 2015, IEEE BECITHCON 2019 & IEEE RAAIACON 2019.<br><br>She has served as an  Editorial board member, IET Signal Processing From 2018 to date and  2018- 20 Member, Technical Committee, Image, Video, and Multimedia (IVM), Asia Pacific Signal and Information Processing Association (APSIPA). She has more than 20 years of experience (20 years as an IEEE volunteer) in leading impactful Technical, Professional, Educational, Industrial, Women Empowerment and Humanitarian Technology, Power and Energy-related Projects at national/international levels.
 							<br> 
 						</p>
 					</div>
